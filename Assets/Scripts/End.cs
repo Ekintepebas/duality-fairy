37a97edf5558873011c6dc89c33bffd6b7c90213
Ekin Tepebas <ekintepebas@gmail.com>
2026-05-17 16:24:48 +0300

    level 1 and scene manager completed

--- Assets/Scripts/End.cs
+++ Assets/Scripts/End.cs
@@ -1,20 +1,36 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class End : MonoBehaviour
-{   
+{
+    [SerializeField] private string nextSceneName = "Level2";
+    [SerializeField] private float fadeDuration = 2f;
+
+    [SerializeField] public Image fadeImage;
     private void OnTriggerEnter2D(Collider2D other)
     {
+        // Make sure it starts fully transparent
+        fadeImage.color = new Color(0, 0, 0, 0);
+        fadeImage.gameObject.SetActive(true);
         Debug.Log("HIT: " + other.name);
+        StartCoroutine(FadeAndLoadScene());
     }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private IEnumerator FadeAndLoadScene()
     {
-        
-    }
+        yield return new WaitForSeconds(1f); // wait 1 second before fading
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        float elapsed = 0f;
+        while (elapsed < fadeDuration)
+        {
+            elapsed += Time.deltaTime;
+            float alpha = Mathf.Clamp01(elapsed / fadeDuration);
+            fadeImage.color = new Color(0, 0, 0, alpha);
+            yield return null;
+        }
+
+        SceneManager.LoadScene(nextSceneName);
     }
 }
--- Assets/Scripts/FadeFlower.cs
+++ Assets/Scripts/FadeFlower.cs
@@ -1,7 +1,43 @@
 using UnityEngine;
-
+using System.Collections;
+using UnityEngine.UI;
 public class FadeFlower : MonoBehaviour
 {
+    public SpriteRenderer[] flowers;
+    public float fadeDuration = 1f;
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+
+        Debug.Log("HITFade: " + other.name);
+        StartCoroutine(FadeRoutine());
+
+    }
+
+    IEnumerator FadeRoutine()
+    {
+        float t = 0f;
+
+        Color[] originalColors = new Color[flowers.Length];
+        Debug.Log("original colors " + originalColors);
+        for (int i = 0; i < flowers.Length; i++)
+            originalColors[i] = flowers[i].color;
+
+        while (t < fadeDuration)
+        {
+            t += Time.deltaTime;
+            float lerp = t / fadeDuration;
+
+            for (int i = 0; i < flowers.Length; i++)
+            {
+                Color grey = Color.Lerp(originalColors[i], Color.grey, lerp);
+                flowers[i].color = grey;
+            }
+
+            yield return null;
+        }
+    }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
